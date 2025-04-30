8ec4c44f8e3e9c390d78d55181beb5f2f6833a9f
Emi536 <paze0410@gmail.com>
2025-04-30 20:26:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,12 @@ if seccion == "👑 Comunidad VIP":
 
         if df is not None:
             df["Fecha"] = pd.to_datetime(df["Fecha"], errors="coerce")
-            df["Hora"] = pd.to_datetime(df["Hora"], errors="coerce").dt.time
+            if "Hora" in df.columns:
+                df["Hora"] = pd.to_datetime(df["Hora"], format="%H:%M:%S", errors="coerce").dt.time
+            else:
+                st.error("❌ La columna 'Hora' no se encuentra en el archivo cargado.")
+                st.stop()
+
             df["Monto"] = pd.to_numeric(df["Monto"], errors="coerce").fillna(0)
             df["Jugador"] = df["Jugador"].astype(str).str.strip()
             df["Jugador_normalizado"] = df["Jugador"].str.lower()
